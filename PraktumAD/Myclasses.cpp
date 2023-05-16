89677649a9e0bb45fb6a56371a735f2b077d8afd
Tim Heinsberg <timheinsberg@web.de>
2023-05-16 14:31:51 +0200

    Initial Commit

---
+++ PraktumAD/Myclasses.cpp
@@ -0,0 +1,44 @@
+
+/* Dateiname: Myclasses.cpp:
+
+Programmkopf mit Programmname, Autor(en)...
+Beschreibung, usw.
+Enthält den Code der Methoden der Klassen POINT, LINE und CIRCLE
+
+*/
+#include "Myclasses.h"
+#include <iostream>
+using namespace std;
+
+//POINT
+
+POINT::POINT(){}
+POINT::POINT (double X, double Y): X(X), Y(Y){}
+POINT::POINT (POINT & other){}
+double POINT::distanceTo(const POINT & other){}  // Abstand zweier POINT-Objekte
+double POINT::getX(){}
+double POINT::getY(){}
+POINT & POINT::operator = (const POINT &other){} // Überladener Zuweisungsoperator
+POINT & POINT::operator + (const POINT &other){} // Überladener Additionsoperator
+POINT & POINT::operator - (const POINT &other){} // Überladener Subtraktionsoperator
+POINT & POINT::operator * (double scalar){}  // Überladener Multiplikationsoperator, POINT * SkalarerWert
+void POINT::show(){
+    cout << "test" << endl;
+} // Ausgabe der Koordinaten in der Form ( x | y )
+POINT::~POINT(){}
+
+
+//LINE
+LINE::LINE (POINT *P1, POINT *P2){}
+POINT * LINE::meets (const LINE *L){}  //Schnittpunkt für zwei Geraden
+bool LINE::parallelTo(const LINE *L){} //Feststellen, ob zwei Geraden parallel sind
+void LINE::show(){}
+
+
+//CIRCLE
+CIRCLE::CIRCLE (POINT *M, double R){} //Initialisierungskonstruktor 1
+CIRCLE::CIRCLE (POINT *A,POINT *B,POINT *C){} //Initialisierungskonstruktor 2
+CIRCLE::POINT* getMiddle(){}
+void CIRCLE::show(){} //Überschriebene Methode show, verwendet show von POINT: Ausgabe des Mittelpunktes und des Radius
+bool CIRCLE::isInCircle(const POINT &){}
+void CIRCLE::meetsOther(CIRCLE *C, int &Anzahl, POINT **S1, POINT **S2){} //C wird übergeben, Anzahl, S1 und S2 kommen zurück
---
+++ PraktumAD/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Myclasses.h"
+using namespace std;
+
+int main()
+{
+    POINT* p = new POINT(2.0,4.0);
+    p->show();
+}
